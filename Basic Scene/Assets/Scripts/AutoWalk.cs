1737c1bcf121d7c3c4c7ef82ac8fd0c7a8c8d8d6
jandzouana <jandzouana@gmail.com>
2017-05-31 00:34:52 -0700

    added light probe group and reflection probes

---
+++ Basic Scene/Assets/Scripts/AutoWalk.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoWalk : MonoBehaviour {
+
+	public Transform vrCamera;
+	public float speed;
+	private float defspeed = 2.25f;
+	private bool moveForward;
+	private CharacterController cc;
+    public bool canMove;
+
+	public void SetMove(bool input){
+		canMove = !input;
+	}
+	// Use this for initialization
+	void Start () {
+		cc = GetComponent<CharacterController> ();
+		//speed = defspeed;
+        canMove = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (moveForward && canMove) {
+
+			Vector3 forward = vrCamera.TransformDirection (Vector3.forward); //checking for forward direction
+			cc.SimpleMove(forward * speed);
+
+		}
+        if ((Input.GetButtonDown("Fire1") || GvrController.AppButtonDown) && canMove && moveForward == false){
+			moveForward = true;
+		}
+        else if ((Input.GetButtonDown("Fire1") || GvrController.AppButtonDown) && canMove && moveForward == true){
+			moveForward = false;
+		}
+
+	}
+}
