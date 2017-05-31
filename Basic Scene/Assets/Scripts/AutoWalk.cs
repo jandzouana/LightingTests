00092ceb8a84935d6828135aa973e66b571cfc53
Jessica Andzouana <jandzouana@gmail.com>
2017-05-31 16:20:30 -0700

    small lighting changes

--- Basic Scene/Assets/Scripts/AutoWalk.cs
+++ Basic Scene/Assets/Scripts/AutoWalk.cs
@@ -29,10 +29,10 @@ public class AutoWalk : MonoBehaviour {
 			cc.SimpleMove(forward * speed);
 
 		}
-        if ((Input.GetButtonDown("Fire1") || GvrController.AppButtonDown) && canMove && moveForward == false){
+        if ((Input.GetButtonDown("Fire1")) && canMove && moveForward == false){
 			moveForward = true;
 		}
-        else if ((Input.GetButtonDown("Fire1") || GvrController.AppButtonDown) && canMove && moveForward == true){
+        else if ((Input.GetButtonDown("Fire1")) && canMove && moveForward == true){
 			moveForward = false;
 		}
 
